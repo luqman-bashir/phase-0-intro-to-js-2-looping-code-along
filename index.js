b1b0c9f255172380770d0aefdc2d6e9bdeb57af0
luqman-bashir <uthamin98@gmail.com>
2024-10-10 20:43:45 +0300

    looping code

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {  
+    const messages = [];   
+    for (let i = 0; i < names.length; i++) {  
+    
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);  
+    }  
+    return messages; 
+  }
+  const result = writeCards(["Charlie", "Samip", "Ali"], "birthday");  
+console.log(result);
+ 
+
+function countDown(start) {  
+    while (start >= 0) {  
+      console.log(start);  
+      start--;  
+    }  
+  }  
+  
+   
+  countDown(5);
